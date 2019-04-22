9b4e5bcbf4846e41a8dc208149f5b6d4244717ca
isavlex <mailsavlex@gmail.com>
2019-04-22 14:41:59 +0300

    add blocks .features .news and .footer

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,9 +1,20 @@
 'use strict';
 
 import sandwichMenu from 'components/navigation/sandwich/sandwich';
+// import featuresSlider from 'components/features/slider';
+import Slider from './plugins/slider';
+
 
 sandwichMenu();
-
+// featuresSlider();
+svg4everybody({
+    nosvg: true, // shiv <svg> and <use> elements and use image fallbacks
+    polyfill: true // polyfill <use> elements for External Content
+});
+let features = new Slider('.features', 559);
+features.sliderInit();
+let news = new Slider('.news__reviews');
+news.sliderInit();
 
 /*
     This file can be used as entry point for webpack!
---
+++ markup/static/js/plugins/slider.js
@@ -0,0 +1,55 @@
+import $ from 'jquery';
+import slick from 'components/slick/slick';
+
+class Slider {
+    constructor(element, breakpoint = false) {
+        this.element = element;
+        this.breakpoint = breakpoint;
+    }
+
+    getSlider() {
+        let element = $(this.element);
+        if (this.breakpoint) {
+            if ($(window).width() <= this.breakpoint) {
+                element.not('.slick-initialized').slick({
+                    infinite: true,
+                    dots: true,
+                    speed: 500,
+                    arrows: false
+                });
+            } else if ($(window).width() > this.breakpoint && element.hasClass('slick-initialized')) {
+                element.slick('unslick');
+            }
+        } else {
+            element.slick({
+                infinite: true,
+                dots: true,
+                speed: 500,
+                appendDots: $('.news__dots'),
+                responsive: [{
+                    breakpoint: 576,
+                    settings: {
+                        arrows: false,
+                    }
+                }, ]
+            });
+        }
+    }
+
+    sliderInit() {
+        if (this.breakpoint) {
+            this.getSlider();
+            $(window).resize(() => {
+                this.getSlider();
+            });
+        } else {
+            this.getSlider();
+        }
+
+    }
+}
+
+
+
+// export default featuresSlider;
+export default Slider;
